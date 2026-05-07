9906bc79cf2e7a23a77a155b6f7c6fabca77d909
Zerinho23 <josearaneda2025@gmail.com>
2026-05-06 22:54:55 -0400

    chore: migrate src/commands/user/perfil.js to PostgreSQL (Neon)

--- src/commands/user/perfil.js
+++ src/commands/user/perfil.js
@@ -6,7 +6,7 @@ module.exports = {
   description: "Muestra tu perfil otaku con nivel, XP y monedas",
   aliases: ["profile", "me"],
   async execute({ sock, msg, sender, from }) {
-    const user = db.getUser(sender);
+    const user = await db.getUser(sender);
     const name = msg.pushName;
     const text = format.formatProfile(user, name);
     await sock.sendMessage(from, { text }, { quoted: msg });
